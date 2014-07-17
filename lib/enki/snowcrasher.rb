c22f6912752c249699d4da5c2eccb2748fa31086
Kostas Karachalios <vrinek@me.com>
2014-07-17 18:18:35 +0200

    Fix a regexp

--- lib/enki/snowcrasher.rb
+++ lib/enki/snowcrasher.rb
@@ -12,7 +12,7 @@ module Enki
 
     def compile_dir(src_dir:, dst_dir:)
       Dir.glob("#{src_dir}/**/*.md").each do |source_file|
-        output_file = "#{dst_dir}/#{source_file[%r{src_dir/(.*)\.md}, 1]}.yml"
+        output_file = "#{dst_dir}/#{source_file[%r{#{src_dir}/(.*)\.md}, 1]}.yml"
         FileUtils.mkdir_p File.dirname(output_file)
 
         compile_file(source: source_file, output: output_file)
